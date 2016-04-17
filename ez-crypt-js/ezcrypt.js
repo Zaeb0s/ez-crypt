2811b35fccccb2061070290e77c5b1525fee4db2
Zaeb0s <christoffer_zakrisson@hotmail.com>
2016-04-17 14:47:57 +0200

    Added RSA

--- ez-crypt-js/ezcrypt.js
+++ ez-crypt-js/ezcrypt.js
@@ -1,6 +1,6 @@
 
 CryptoJS = require('crypto-js');
-
+forge = require('node-forge');
 
 exports.Crypt = function(key, iv, mode, padding){
     this.key_str = key || random_hex(32);
@@ -62,3 +62,74 @@ exports.Crypt = function(key, iv, mode, padding){
         return result;
     }
 };
+
+exports.RSA = new (function RSA(){ //TODO: Add function for importing keys
+    this.generate_key = function generate_key(bits, e){
+        bits = bits || 2048;
+        e = e || 65537;
+        return forge.pki.rsa.generateKeyPair({bits: bits, e: e});
+    };
+
+    this.encrypt = function encrypt(data, key, type){
+        type = type || 'RSA-OAEP';
+        encrypted = key.publicKey.encrypt(data, type);
+        return str2hex(encrypted);
+    };
+
+    this.decrypt = function encrypt(data, key, type){
+        type = type || 'RSA-OAEP';
+
+        return key.privateKey.decrypt(hex2str(data), type);
+    };
+
+    this.sign = function sign(data, key){
+        var md = forge.md.sha256.create();
+        md.update(data, 'utf8');
+        return key.privateKey.sign(md);
+    };
+
+    this.verify = function verify(data, signature, key){
+        var md = forge.md.sha256.create();
+        md.update(data, 'urf8');
+        key.publicKey.verify(md.digest().bytes(), signature)
+    };
+    
+    this.exportKey(key) = function exportKey{
+        forge.pki.privateKeyToPem(key.privateKey)
+    };
+    
+    this.importKey(pem) = function importKey{
+        forge.pki.privateKeyFromPem(pem)
+    }
+})();
+
+
+function str2hex(string){
+    var hex, i;
+
+    var result = "";
+    for (i=0; i<string.length; i++) {
+        hex = string.charCodeAt(i).toString(16);
+        result += ("000"+hex).slice(-4);
+    }
+    return result
+}
+
+function hex2str(hex){
+    var j;
+    var hexes = hex.match(/.{1,4}/g) || [];
+    var back = "";
+    for(j = 0; j<hexes.length; j++) {
+        back += String.fromCharCode(parseInt(hexes[j], 16));
+    }
+    return back;
+}
+
+
+
+
+
+
+
+
+
--- ez-crypt-js/test.js
+++ ez-crypt-js/test.js
@@ -10,3 +10,16 @@ console.log('Key: ' + crypt.key_str);
 console.log('IV: ' + crypt.iv_str);
 console.log('Encrypted string: ' + encrypted);
 console.log('Decrypted string: ' + decrypted);
+
+
+
+
+
+var string = 'Hello, world',
+    key = ezcrypt.RSA.generate_key(),
+    encrypted = ezcrypt.RSA.encrypt(string, key);
+    decrypted = ezcrypt.RSA.decrypt(encrypted, key);
+
+console.log('String in encrypt: ' + string);
+console.log('Encrypted string: ' + encrypted);
+console.log('Decrypted string: ' + decrypted);
